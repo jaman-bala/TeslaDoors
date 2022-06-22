4a97b8fc19acdb58593549f1fb309107ac0ed4ed
jaman-bala <ermekoffdastan@yandex.ru>
2022-06-22 11:29:52 +0600

    edit sattings staticfile

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 SECRET_KEY = 'django-insecure-u6b9pozqpg*x=&$i-+6dukh3_)v(akz6$1i($+a72*!6(r)&3k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG", default=True)
 
 ALLOWED_HOSTS = ['*']
 
@@ -126,13 +126,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
+# STATIC_URL = '/static/'
+# if DEBUG:
+#     STATICFILES_DIRS = [
+#         BASE_DIR / "static",
+#     ]
+# else:
+#     STATIC_ROOT = os.path.join(BASE_DIR, "static")
+#     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+#     MEDIA_URL = "/media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
