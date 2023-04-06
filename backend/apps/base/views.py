c8baf95ef8f18053bc3686575082b08d7737485f
Jamanbala_it <ermekoffdastan@gmail.com>
2023-04-06 16:12:17 +0600

    add css and new app

---
+++ backend/apps/base/views.py
@@ -0,0 +1,48 @@
+from django.urls import reverse_lazy
+from django.views.generic import FormView, ListView, DeleteView
+from django.shortcuts import redirect, render
+from .models import Product
+from .forms import ProductForm
+
+
+class Index(FormView, ListView):
+    model = Product
+    form_class = ProductForm
+    context_object_name = 'informations'
+    template_name = 'index.html'
+    success_url = reverse_lazy('index')
+
+
+    def form_valid(self, form):
+        data = {}
+        if self.request.method == 'POST':
+            form = ProductForm(self.request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('repos')
+        else:
+            error = 'Формат был неверной'
+            form = ProductForm()
+            data = {
+                'form': form,
+                'error': error
+        }
+        return render(self.request, 'repos.html', data)
+
+
+class IndexDetail(DeleteView):
+    model = Product.objects.all()
+    template_name = 'code.html'
+    context_object_name = 'code'
+    success_url = reverse_lazy('code')
+    queryset = Product.objects.all()
+
+
+class ReposView(ListView):
+    model = Product
+    context_object_name = 'informationsi'
+    template_name = 'repos.html'
+    success_url = reverse_lazy('repos')
+    paginate_by = 65
+
+
---
+++ backend/config/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('my_admin_site/', admin.site.urls),
+    path('', include('backend.apps.account.urls')),
+    path('base/', include('backend.apps.base.urls')),
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
