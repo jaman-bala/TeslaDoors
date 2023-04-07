3533a298476153a83b16927f3ca02e058ed089e9
Jamanbala_it <ermekoffdastan@gmail.com>
2023-04-07 19:37:45 +0600

    add error404

--- backend/apps/account/views.py
+++ backend/apps/account/views.py
@@ -18,11 +18,15 @@ def sign_in(request):
                     login(request, user)
                     return redirect('index')
                 else:
-                    return HttpResponse("Аккаунт неактивирован")
+                    return redirect('error')
             else:
-                return HttpResponse("Неправильные данные пользователя")
+                return redirect('error')
 
     else:
         form = LoginForm()
 
     return render(request, 'login.html', {"form": form})
+
+
+def error(request):
+    return render (request, 'error404.html')
